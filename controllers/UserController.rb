239dda12d9cefb685c2d4083171a45eeef7003a1
Ryne Laster <ryne.laster@gmail.com>
2018-02-05 10:49:55 -0600

    Login and register has been completed

--- controllers/UserController.rb
+++ controllers/UserController.rb
@@ -13,11 +13,28 @@ class UserController < ApplicationController
 	end
 
 	post '/login' do
-		params.to_json
+		@user = User.find_by(username: params[:username])
+
+		if @user && @user.password == params[:password]
+			session[:username] = @user.username
+			session[:logged_in] = true
+			session[:message] = "Logged in as #{@user.username}"
+			redirect '/items'
+		else
+			session[:message] = "Invalid username or password."
+			redirect '/user/login'
+		end
 	end
 
 	post '/register' do
-		params.to_json
+		@user = User.new
+		@user.username = params[:username]
+		@user.password = params[:password]
+		@user.save
+		session[:logged_in] = true
+		session[:username] = @user.username
+		session[:message] = "Thank you for registering (as #{@user.username}).  Enjoy the site!"
+		redirect '/items'
 	end
 
 end
